b43b6a4df6c8421572cf5cca8dbbf2a5e75946b0
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-23 01:11:36 -0600

    Adding layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,22 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>Little feed</title>
+        <meta name="description" content="Little feed" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex h-screen justify-center">
+        <div className="w-full h-full border-slate-400 md:max-w-2xl border-x overflow-y-scroll">
+          <Component {...pageProps} />
+        </div>
+      </main>
       <Toaster position="top-right" />
-      <Component {...pageProps} />
     </ClerkProvider>
   );
 };
