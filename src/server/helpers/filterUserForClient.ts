259bb5e773c49611ee0ee184e7020962e2a0ea62
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-23 01:12:28 -0600

    server helper

---
+++ src/server/helpers/filterUserForClient.ts
@@ -0,0 +1,13 @@
+import { User } from "@clerk/nextjs/dist/server";
+
+const filterUserForClient = (user: User) => {
+    return {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        profileImageUrl: user.profileImageUrl,
+    };
+}
+
+export default filterUserForClient;
