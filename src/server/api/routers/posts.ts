26257af7cdef2ed2ff8521075a741367a9b8be7b
Sebastian Vargas R <s.varggas@gmail.com>
2023-05-23 01:12:16 -0600

    Setting up user profile

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,21 +1,34 @@
 import { clerkClient } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/dist/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
+import filterUserForClient from "~/server/helpers/filterUserForClient";
+import { Post } from "@prisma/client";
 
-const filterUserForClient = (user: User) => {
-    return {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        username: user.username,
-        profileImageUrl: user.profileImageUrl,
-    };
-}
+const addUserDataToPosts = async (posts: Post[]) => {
+    const users = (
+        await clerkClient.users.getUserList({
+            userId: posts.map((post) => post.authorId),
+            limit: 100,
+        })
+    ).map((user) => filterUserForClient(user));
+
+    return posts.map((post) => {
+        const author = users.find((user) => user.id === post.authorId);
+
+        if (!author || !author.username) {
+            throw new TRPCError({
+                code: "INTERNAL_SERVER_ERROR",
+                message: "Author not found",
+            });
+        }
+
+        return { post, author };
+    });
+};
 
 // Create a new ratelimiter, that allows 3 requests per 1 min
 const ratelimit = new Ratelimit({
@@ -39,25 +52,7 @@ export const postRouter = createTRPCRouter({
             },
         });
 
-        const users = (
-            await clerkClient.users.getUserList({
-                userId: posts.map((post) => post.authorId),
-                limit: 100,
-            })
-        ).map((user) => filterUserForClient(user));
-
-        return posts.map((post) => {
-            const author = users.find((user) => user.id === post.authorId);
-
-            if (!author || !author.username) {
-                throw new TRPCError({
-                    code: "INTERNAL_SERVER_ERROR",
-                    message: "Author not found",
-                });
-            }
-
-            return { post, author };
-        });
+        return addUserDataToPosts(posts);
     }),
 
     create: privateProcedure
@@ -86,4 +81,20 @@ export const postRouter = createTRPCRouter({
             });
             return post;
         }),
+
+    getPostsByUserId: publicProcedure.input(
+        z.object({
+            userId: z.string(),
+        })
+    )
+        .query(({ input, ctx }) => ctx.prisma.post.findMany({
+            where: {
+                authorId: input.userId,
+            },
+            orderBy: {
+                createdAt: "desc",
+            },
+            take: 100,
+        }).then(addUserDataToPosts)
+        )
 });
